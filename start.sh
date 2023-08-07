f59d781d7165f41bf50deada56fd554936690ac0
SandeepSamba <sandeep.sujitha@gmail.com>
2023-08-07 13:59:22 +0530

    add supervisor

--- start.sh
+++ start.sh
@@ -6,13 +6,17 @@ DEFAULT_PASSWORD="$(cat /etc/connector/config.json | jq -r '.password')"
 
 #PASSWORD="${SET_PASSWORD:-$DEFAULT_PASSWORD}"
 
-flask run --port 6060 &
+# flask run --port 6060 &
 
 # set htpasswd for nginx basic auth
 htpasswd -db -c /etc/nginx/.htpasswd hasura "$DEFAULT_PASSWORD"
 
-nginx -c "$PWD/nginx.conf" &
+# nginx -c "$PWD/nginx.conf" &
 
-cd /notebook && jupyter notebook --allow-root --config /jupyter_notebook_config.py &
+# cd /notebook && jupyter notebook --allow-root --config /jupyter_notebook_config.py &
 
-wait -n
+# wait -n
+
+supervisord -c /supervisor/supervisord.conf -n &
+SUPERVISOR_PID=$!
+wait $SUPERVISOR_PID
