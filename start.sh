01ad008b4d793b1d11f91c20a243a4947fc41620
Sandeep Raj kumar Kandasamy <sandeep@hasura.io>
2023-08-14 16:33:17 +0530

    trial commit 1

--- start.sh
+++ start.sh
@@ -1,5 +1,31 @@
 #!/usr/bin/env bash
-set -e
+set -o pipefail
+
+mkdir -p "$MNT_DIR"
+
+# don't mount GCS Fuse in local dev
+if [ "$LOCAL_DEV" == "yes" ]; then
+  echo "Local dev is configured, don't setup fuse"
+  cp -r /notebook "$MNT_DIR"
+else
+  echo "Local dev is not setup, configure fuse"
+  # check if file exist
+  gsutil -q stat "gs://$K_SERVICE/notebook/server.ipynb"
+
+  PATH_EXIST=$?
+  if [ ${PATH_EXIST} -eq 0 ]; then
+    echo "bucket exist"
+  else
+    echo "bucket does not exist"
+    gsutil -m cp -r /notebook "gs://$K_SERVICE/notebook"
+  fi
+
+  echo "Mounting GCS Fuse."
+  echo "Mounting Bucket $K_SERVICE at $MNT_DIR"
+  gcsfuse --implicit-dirs --debug_http --debug_gcs --debug_fuse "$K_SERVICE" "$MNT_DIR"
+  #gcsfuse --implicit-dirs --only-dir "$K_SERVICE/" --debug_http --debug_gcs --debug_fuse $BUCKET $MNT_DIR
+  echo "Mounting completed."
+fi
 
 #SET_PASSWORD="$(cat /etc/connector/config.json | jq -r '.password')"
 DEFAULT_PASSWORD="$(cat /etc/connector/config.json | jq -r '.password')"
